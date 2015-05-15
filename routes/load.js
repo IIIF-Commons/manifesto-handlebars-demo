c39547c58e8ae4b3b353744c36c360d56f3e365b
edsilv <e.silverton@gmail.com>
2015-05-15 13:06:39 +0100

    can now pass manifest url to load

--- routes/load.js
+++ routes/load.js
@@ -2,12 +2,13 @@ var express = require('express');
 var router = express.Router();
 var manifesto = require('manifesto');
 
-router.get('/', function(req, res, next) {
+router.get('/:url', function(req, res, next) {
 
     var title = manifesto.sayHello("from node");
 
-    //http://wellcomelibrary.org/iiif/b18035723/manifest
-    manifesto.load("http://localhost:3000/data/manifest.json", function(manifest) {
+    var url = decodeURIComponent(req.params.url);
+
+    manifesto.load(url, function(manifest) {
 
         var m = JSON.parse(manifest);
 
