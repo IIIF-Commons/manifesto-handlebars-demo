0d62d904c0b9a45b55b5b39e0ddc7ade897702c3
edsilv <e.silverton@gmail.com>
2015-06-28 23:03:50 +0100

    added bump

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,11 @@
-var argv = require('yargs').argv,
-    bump = require('gulp-bump'),
-    Config = require('./gulpfile.config'),
+var Config = require('./gulpfile.config'),
+    config = new Config(),
     exec = require('child_process').exec,
     gulp = require('gulp'),
-    open = require('open');
-
-var config = new Config();
-
-gulp.task('bump', function(){
-    var bumpType = argv.type || 'patch'; // major.minor.patch
-
-    gulp.src(['./bower.json', './package.json'])
-        .pipe(bump({type: bumpType}))
-        .pipe(gulp.dest('./'));
-});
-
-// requires global gulp-cli
-gulp.task('bump:minor', function(cb){
-    exec('gulp bump --type minor', function (err, stdout, stderr) {
-        cb();
-    });
-});
-
-// requires global gulp-cli
-gulp.task('bump:major', function(cb){
-    exec('gulp bump --type major', function (err, stdout, stderr) {
-        cb();
-    });
-});
+    open = require('open'),
+    requireDir = require('require-dir'),
+    runSequence = require('run-sequence'),
+    tasks = requireDir('./tasks');
 
 gulp.task('serve', function(){
     exec('npm start', function (err, stdout, stderr) {
